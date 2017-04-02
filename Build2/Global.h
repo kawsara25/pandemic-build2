99af72eb9a91a43b23287f8a081b511fbf641106
Jasmine <jasmine.leblondc@gmail.com>
2017-04-02 19:59:38 -0400

    Files reorganization to make Player has a pawn. Pawn has a city. (graph and save files still need to be modified)

---
+++ Build2/Global.h
@@ -0,0 +1,200 @@
+//**********************************************
+//COMP345 BUILD 1
+//Winter 2017
+//Team Buffalos
+//**********************************************
+//
+//Global.h
+
+
+#ifndef Global_h
+#define Global_h
+
+
+//----------- GLOBAL OBJECTS---------------------------------------------------->>>
+//Contains only global objects (cards, pawns, player hands, array of players, array of playerviews, playerdeck, discard pile, etc.)
+
+
+const int nbplayers = 2;        // # Players to be instantiated ( 2 to 4)
+const int nbcardsplayer = 4;    // # Cards per player when game starts
+const int nbplayercards = 59;   // # PlayerCard's in the whole game
+const int diseaseCubesCount = 24; // # of disease cubes per color disease
+
+//Infection rate, initialized to 1
+int infectionRate = 1;
+//remaining disease cubes (blue, yellow, black, red)
+int remainingDiseaseCubes[4] = {diseaseCubesCount,diseaseCubesCount,diseaseCubesCount,diseaseCubesCount };
+
+//Creates 4 reference cards needed in the game, put them in an array for access
+RefCard refcard1, refcard2, refcard3, refcard4;
+RefCard referencecards[] = { refcard1, refcard2, refcard3, refcard4 };
+
+//Pawn pink("Pink"), orange("Orange"), white ("White"), brown("Brown"), palegreen("Pale Green"), darkgreen("Dark Green"), aqua("Aqua");
+
+//Creates Role Cards (inherited from Cards)
+DispatcherCard dispatchercard1;
+MedicCard mediccard1;
+ScientistCard scientistcard1;
+ResearcherCard researchercard1;
+OperationsexpertCard operationsexpertcard1;
+QuarantinespecialistCard quarantinespecialistcard1;
+ContingencyplannerCard contingencyplannercard1;
+
+
+//Creates player hands (creates many vectors of *PlayerCard)
+//-----hands is an attribute from class Player-----
+std::vector<PlayerCard*> dispatcherhand, medichand, scientisthand, researcherhand, operationsexperthand, quarantinespecialisthand, contingencyplannerhand;
+
+//Creates <vector> playerdeck that contains *PlayerCard
+std::vector<PlayerCard*> playerdeck;
+
+//Creates <vector> discardpile that contains *PlayerCard
+std::vector <PlayerCard*> discardpile;
+
+//Creates <vector> infectiondeck that contains *InfectionCard
+std::vector <InfectionCard*> infectiondeck;
+
+//Creates <vector> infectiondeck that contains *InfectionCard DISCARD PILE
+std::vector <InfectionCard*> infectiondeck_discard;
+
+//Creates Array of *Players
+Player* arrayofPlayers[nbplayers];
+
+//Creates Array of *PlayerView objects (Observers) [1 for each player]
+PlayerView * arrayofPlayerViews[nbplayers];
+
+
+//-----------PlayerCard CARDS------------------------------->
+//----------Epidemic cards----------
+EpidemicCard* epcard1=new EpidemicCard();
+EpidemicCard* epcard2=new EpidemicCard();
+EpidemicCard* epcard3=new EpidemicCard();
+EpidemicCard* epcard4=new EpidemicCard();
+EpidemicCard* epcard5=new EpidemicCard();
+
+EpidemicCard* epcardarr[]={epcard1, epcard2, epcard3, epcard4, epcard5};
+
+//----------Event cards----------
+Airlift* airliftcard=new Airlift();
+OneQuietNight* onequietnightcard=new OneQuietNight();
+GovernmentGrant* governmentgrantcard=new GovernmentGrant();
+ResilientPopulation* resilientpopulationcard=new ResilientPopulation();
+Forecast* forecastcard=new Forecast();
+
+EventCard* eventcardarr[]={airliftcard, onequietnightcard, governmentgrantcard, resilientpopulationcard, forecastcard};
+
+//---------City cards----------
+
+//Blue
+BlueCity* sanfrancisco=new BlueCity("sanfrancisco", "San Francisco", "", "");
+BlueCity* chicago=new BlueCity("chicago", "Chicago", "", "");
+BlueCity* atlanta=new BlueCity("atlanta", "Atlanta", "", "");
+BlueCity* montreal=new BlueCity("montreal", "Montreal", "", "");
+BlueCity* washington=new BlueCity("washington", "Washington", "", "");
+BlueCity* newyork=new BlueCity("newyork", "New York", "", "");
+BlueCity* londres=new BlueCity("londres", "Londres", "", "");
+BlueCity* madrid=new BlueCity("madrid", "Madrid", "", "");
+BlueCity* paris=new BlueCity("paris", "Paris", "", "");
+BlueCity* milan=new BlueCity("milan", "Milan", "", "");
+BlueCity* essen=new BlueCity("essen", "Essen", "", "");
+BlueCity* stpetersbourg=new BlueCity("stpetersbourg", "St Petersbourg", "", "");
+//Yellow
+YellowCity* losangeles=new YellowCity("losangeles", "Los Angeles", "", "");
+YellowCity* mexico=new YellowCity("mexico", "Mexico", "", "");
+YellowCity* miami=new YellowCity("miami", "Miami", "", "");
+YellowCity* bogota=new YellowCity("bogota", "Bogota", "", "");
+YellowCity* lima=new YellowCity("lima", "Lima", "", "");
+YellowCity* santiago=new YellowCity("santiago", "Santiago", "",  "");
+YellowCity* buesnosaires=new YellowCity("buenosaires", "Buenos Aires", "", "");
+YellowCity* saopaulo=new YellowCity("saopaulo", "Sao Paulo", "", "");
+YellowCity* lagos=new YellowCity("lagos", "Lagos", "", "");
+YellowCity* kinshasa=new YellowCity("kinshasa", "Kinshasa", "" , "");
+YellowCity* johannesburg=new YellowCity("johannesburg", "Johannesburg", "", "");
+YellowCity* khartoum=new YellowCity("khartoum", "Khartoum", "", "");
+//Black
+BlackCity* moscou=new BlackCity("moscou", "Moscou", "", "");
+BlackCity* istanbul=new BlackCity("istanbul", "Istanbul", "", "");
+BlackCity* alger=new BlackCity("alger", "Alger", "", "");
+BlackCity* lecaire=new BlackCity("lecaire", "Le Caire", "", "");
+BlackCity* bagdad=new BlackCity("bagdad", "Bagdad", "", "");
+BlackCity* teheran=new BlackCity("teheran", "Teheran", "", "");
+BlackCity* karachi=new BlackCity("karachi", "Karachi", "", "");
+BlackCity* ryad=new BlackCity("ryad", "Ryad", "", "");
+BlackCity* delhi=new BlackCity("delhi", "Delhi", "", "");
+BlackCity* mumbai=new BlackCity("mumbai", "Mumbai", "", "");
+BlackCity* chennai=new BlackCity("chennai", "Chennai", "", "");
+BlackCity* calcutta=new BlackCity("calcutta", "Calcutta", "", "");
+//Red
+RedCity* pekin=new RedCity("pekin", "Pekin", "", "");
+RedCity* seoul=new RedCity("seoul", "Seoul", "", "");
+RedCity* tokyo=new RedCity("tokyo", "Tokyo", "", "");
+RedCity* shanghai=new RedCity("shanghai", "Shanghai", "", "");
+RedCity* osaka=new RedCity("osaka", "Osaka", "", "");
+RedCity* taipei=new RedCity("taipei", "Taipei", "", "");
+RedCity* hongkong=new RedCity("hongkong", "Hong Kong", "", "");
+RedCity* manille=new RedCity("manille", "Manille", "", "");
+RedCity* bangkok=new RedCity("bangkok", "Bangkok", "", "");
+RedCity* hochiminhville=new RedCity("hochiminhville", "Ho Chi Minh Ville", "", "");
+RedCity* jakarta=new RedCity("jakarta", "Jakarta", "", "");
+RedCity* sydney=new RedCity("sydney", "Sydney", "", "");
+
+CityCard* citycardarr[]={sanfrancisco, chicago, atlanta, montreal, washington, newyork, londres, madrid, paris, milan, essen, stpetersbourg, losangeles, mexico, miami, bogota, lima, santiago, buesnosaires, saopaulo, lagos, kinshasa, johannesburg, khartoum, moscou, istanbul, alger, lecaire, bagdad, teheran, karachi, ryad, delhi, mumbai, chennai, calcutta, pekin, seoul, tokyo, shanghai, osaka, taipei, hongkong, manille, bangkok, hochiminhville, jakarta, sydney};
+
+//---------Infection cards----------
+InfectionCard* sanfrancisco_inf = new InfectionCard("sanfrancisco", "San Francisco", "blue", "");
+InfectionCard* chicago_inf = new InfectionCard("chicago", "Chicago", "blue", "");
+InfectionCard* atlanta_inf = new InfectionCard("atlanta", "Atlanta", "blue", "");
+InfectionCard* montreal_inf = new InfectionCard("montreal", "Montreal", "blue", "");
+InfectionCard* washington_inf = new InfectionCard("washington", "Washington", "blue", "");
+InfectionCard* newyork_inf = new InfectionCard("newyork", "New York", "blue", "");
+InfectionCard* londres_inf = new InfectionCard("londres", "Londres", "blue", "");
+InfectionCard* madrid_inf = new InfectionCard("madrid", "Madrid", "blue", "");
+InfectionCard* paris_inf = new InfectionCard("paris", "Paris", "blue", "");
+InfectionCard* milan_inf = new InfectionCard("milan", "Milan", "blue", "");
+InfectionCard* essen_inf = new InfectionCard("essen", "Essen", "blue", "");
+InfectionCard* stpetersbourg_inf = new InfectionCard("stpetersbourg", "St Petersbourg", "blue", "");
+//Yellow
+InfectionCard* losangeles_inf = new InfectionCard("losangeles", "Los Angeles", "yellow", "");
+InfectionCard* mexico_inf = new InfectionCard("mexico", "Mexico", "yellow", "");
+InfectionCard* miami_inf = new InfectionCard("miami", "Miami", "yellow", "");
+InfectionCard* bogota_inf = new InfectionCard("bogota", "Bogota", "yellow", "");
+InfectionCard* lima_inf = new InfectionCard("lima", "Lima", "yellow", "");
+InfectionCard* santiago_inf = new InfectionCard("santiago", "Santiago", "yellow", "");
+InfectionCard* buesnosaires_inf = new InfectionCard("buenosaires", "Buenos Aires", "yellow", "");
+InfectionCard* saopaulo_inf = new InfectionCard("saopaulo", "Sao Paulo", "yellow", "");
+InfectionCard* lagos_inf = new InfectionCard("lagos", "Lagos", "yellow", "");
+InfectionCard* kinshasa_inf = new InfectionCard("kinshasa", "Kinshasa", "yellow", "");
+InfectionCard* johannesburg_inf = new InfectionCard("johannesburg", "Johannesburg", "yellow", "");
+InfectionCard* khartoum_inf = new InfectionCard("khartoum", "Khartoum", "yellow", "");
+//Black
+InfectionCard* moscou_inf = new InfectionCard("moscou", "Moscou", "black", "");
+InfectionCard* istanbul_inf = new InfectionCard("istanbul", "Istanbul", "black", "");
+InfectionCard* alger_inf = new InfectionCard("alger", "Alger", "black", "");
+InfectionCard* lecaire_inf = new InfectionCard("lecaire", "Le Caire", "black", "");
+InfectionCard* bagdad_inf = new InfectionCard("bagdad", "Bagdad", "black", "");
+InfectionCard* teheran_inf = new InfectionCard("teheran", "Teheran", "black", "");
+InfectionCard* karachi_inf = new InfectionCard("karachi", "Karachi", "black", "");
+InfectionCard* ryad_inf = new InfectionCard("ryad", "Ryad", "black", "");
+InfectionCard* delhi_inf = new InfectionCard("delhi", "Delhi", "black", "");
+InfectionCard* mumbai_inf = new InfectionCard("mumbai", "Mumbai", "black", "");
+InfectionCard* chennai_inf = new InfectionCard("chennai", "Chennai", "black", "");
+InfectionCard* calcutta_inf = new InfectionCard("calcutta", "Calcutta", "black", "");
+//Red
+InfectionCard* pekin_inf = new InfectionCard("pekin", "Pekin", "red", "");
+InfectionCard* seoul_inf = new InfectionCard("seoul", "Seoul", "red", "");
+InfectionCard* tokyo_inf = new InfectionCard("tokyo", "Tokyo", "red", "");
+InfectionCard* shanghai_inf = new InfectionCard("shanghai", "Shanghai", "red", "");
+InfectionCard* osaka_inf = new InfectionCard("osaka", "Osaka", "red", "");
+InfectionCard* taipei_inf = new InfectionCard("taipei", "Taipei", "red", "");
+InfectionCard* hongkong_inf = new InfectionCard("hongkong", "Hong Kong", "red", "");
+InfectionCard* manille_inf = new InfectionCard("manille", "Manille", "red", "");
+InfectionCard* bangkok_inf = new InfectionCard("bangkok", "Bangkok", "red", "");
+InfectionCard* hochiminhville_inf = new InfectionCard("hochiminhville", "Ho Chi Minh Ville", "red", "");
+InfectionCard* jakarta_inf = new InfectionCard("jakarta", "Jakarta", "red", "");
+InfectionCard* sydney_inf = new InfectionCard("sydney", "Sydney", "red", "");
+
+InfectionCard* infectioncardarr[] = { sanfrancisco_inf,chicago_inf,atlanta_inf,montreal_inf, washington_inf, newyork_inf, londres_inf,madrid_inf,paris_inf,milan_inf,essen_inf,stpetersbourg_inf,losangeles_inf, mexico_inf, miami_inf, bogota_inf, lima_inf, santiago_inf, buesnosaires_inf, saopaulo_inf, lagos_inf, kinshasa_inf, johannesburg_inf, khartoum_inf, moscou_inf, istanbul_inf, alger_inf, lecaire_inf, bagdad_inf, teheran_inf, karachi_inf, ryad_inf, delhi_inf, mumbai_inf, chennai_inf, calcutta_inf, pekin_inf, seoul_inf, tokyo_inf, shanghai_inf, osaka_inf, taipei_inf, hongkong_inf, manille_inf, bangkok_inf, hochiminhville_inf, jakarta_inf, sydney_inf };
+
+// <<<-----------------------global objects--------------------------------------------------
+
+#endif /* Global_h */
---
+++ Build2/main.cpp
@@ -0,0 +1,279 @@
+//**********************************************
+//COMP345 BUILD 1
+//Winter 2017
+//Team Buffalos
+//**********************************************
+//
+//main.cpp
+
+#include <iostream>
+#include <ctime>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+#include <vector>
+#include <algorithm>
+#include <random>
+
+#include "PlayerView.h"
+//#include "CardsHeaders.h" (through PlayerView - Player - Subject - CardsHeaders)
+//#include "SaveLoad.h"
+#include "Global.h"
+
+
+//ONLY FOR TEST (TO BE REMOVED)
+City* atlantacity = new City("Atlanta");
+
+
+void initInfectionDeck() {
+    infectiondeck.insert(infectiondeck.end(), infectioncardarr, infectioncardarr + (sizeof(infectioncardarr) / sizeof(infectioncardarr[0])));
+    
+    shuffle(infectiondeck.begin(), infectiondeck.end(), std::default_random_engine(std::random_device()()));
+}
+void setInitPlayerDeck(){
+    playerdeck.insert(playerdeck.end(), citycardarr, citycardarr+(sizeof(citycardarr)/sizeof(citycardarr[0])));
+    playerdeck.insert(playerdeck.end(), eventcardarr, eventcardarr+(sizeof(eventcardarr)/sizeof(eventcardarr[0])));
+    
+    //Rnd Shuffle playerdeck
+    shuffle(playerdeck.begin(), playerdeck.end(), std::default_random_engine(std::random_device()()));
+    //std::random_shuffle(playerdeck.begin(), playerdeck.end());
+    //    for(int i=0; i<playerdeck.size();i++){
+    //        playerdeck[i]->printCard();
+    //    }
+    
+    
+}
+
+void createRoles(){
+    // Method for distributing roles
+    srand(static_cast<unsigned int>(time(nullptr)));
+    int arrcheck[nbplayers];
+    for (int i = 0; i < nbplayers; i++) {
+        arrcheck[i] = nbplayers + 2;		// initialize test array elements with values
+        // nbplayers + 2  just to make sure that this value is never reached
+    }
+    
+    for (int i = 0; i < nbplayers; i++) {		// Method for giving role (by number)
+        bool check;
+        int nb;
+        do {
+            nb = rand() % 6;				// rnd nb goes from 0 to 6
+            check = true;
+            for (int j = 0; j <= i; j++) {
+                if (nb == arrcheck[j]) {
+                    check = false;
+                    break;
+                }
+            }
+            
+        } while(!check);
+        arrcheck[i] = nb;
+        // std::cout<<arrcheck[i]<<std::endl;    // test method output rand nb
+    }
+    
+    
+    // FOR EACH PAWN (PLAYER)...
+    // Distributes actual role with switch(rndnumber)
+    // Call corresponding Role Player constructor
+    // then distribute PlayerCard's from the <vector> playerdeck to the <vector> player hand
+    // arrayOfPlayer[i] points to the role player object created to keep track of all players
+    // creates playerview object (observer) for each player (subject)
+    // Set playercard*'s in player_hand's  - Then NOTIFY();
+    for (int i = 0; i < nbplayers; i++) {
+        
+        
+        
+        switch(arrcheck[i]){
+            case 0:{
+                Pawn dispatcherpawn("pink", atlantacity);
+                Dispatcher* dispatcher = new Dispatcher(&dispatcherpawn, &referencecards[i], &dispatchercard1, dispatcherhand);
+                for (int k = 0; k < nbcardsplayer; k++) {
+                    dispatcherhand.push_back(playerdeck.back());
+                    playerdeck.pop_back();
+                }
+                arrayofPlayerViews[i] = new PlayerView(dispatcher);
+                dispatcher->setHand(dispatcherhand);
+                arrayofPlayers[i]=dispatcher;
+                break;
+            }
+            case 1:{
+                Pawn medicpawn("orange", atlantacity);
+                Medic* medic=new Medic(&medicpawn, &referencecards[i], &mediccard1, medichand);
+                for(int k=0;k<nbcardsplayer;k++){
+                    medichand.push_back(playerdeck.back());
+                    playerdeck.pop_back();
+                }
+                arrayofPlayerViews[i] = new PlayerView(medic);
+                medic->setHand(medichand);
+                arrayofPlayers[i]=medic;
+                break;
+            }
+            case 2:{
+                Pawn scientistpawn("white", atlantacity);
+                Scientist* scientist=new Scientist(&scientistpawn, &referencecards[i], &scientistcard1, scientisthand);
+                for(int k=0;k<nbcardsplayer;k++){
+                    scientisthand.push_back(playerdeck.back());
+                    playerdeck.pop_back();
+                }
+                arrayofPlayerViews[i] = new PlayerView(scientist);
+                scientist->setHand(scientisthand);
+                arrayofPlayers[i]=scientist;
+                break;
+            }
+            case 3:{
+                Pawn researcherpawn("brown", atlantacity);
+                Researcher* researcher=new Researcher(&researcherpawn, &referencecards[i], &researchercard1, researcherhand);
+                for(int k=0;k<nbcardsplayer;k++){
+                    researcherhand.push_back(playerdeck.back());
+                    playerdeck.pop_back();
+                }
+                arrayofPlayerViews[i] = new PlayerView(researcher);
+                researcher->setHand(researcherhand);
+                arrayofPlayers[i]=researcher;
+                break;
+            }
+            case 4:{
+                Pawn operationsexpertpawn("palegreen", atlantacity);
+                Operationsexpert* operationsexpert=new Operationsexpert(&operationsexpertpawn, &referencecards[i], &operationsexpertcard1, operationsexperthand);
+                for(int k=0;k<nbcardsplayer;k++){
+                    operationsexperthand.push_back(playerdeck.back());
+                    playerdeck.pop_back();
+                }
+                arrayofPlayerViews[i] = new PlayerView(operationsexpert);
+                operationsexpert->setHand(operationsexperthand);
+                arrayofPlayers[i]=operationsexpert;
+                break;
+            }
+            case 5:{
+                Pawn quarantinespecialistpawn("darkgreen", atlantacity);
+                Quarantinespecialist* quarantinespecialist=new Quarantinespecialist(&quarantinespecialistpawn, &referencecards[i], &quarantinespecialistcard1, quarantinespecialisthand);
+                for(int k=0;k<nbcardsplayer;k++){
+                    quarantinespecialisthand.push_back(playerdeck.back());
+                    playerdeck.pop_back();
+                }
+                arrayofPlayerViews[i] = new PlayerView(quarantinespecialist);
+                quarantinespecialist->setHand(quarantinespecialisthand);
+                arrayofPlayers[i]=quarantinespecialist;
+                break;
+            }
+            case 6:{
+                Pawn contingencyplannerpawn("aqua", atlantacity);
+                Contingencyplanner* contingencyplanner=new Contingencyplanner(&contingencyplannerpawn, &referencecards[i], &contingencyplannercard1,  contingencyplannerhand);
+                for(int k=0;k<nbcardsplayer;k++){
+                    contingencyplannerhand.push_back(playerdeck.back());
+                    playerdeck.pop_back();
+                }
+                arrayofPlayerViews[i] = new PlayerView(contingencyplanner);
+                contingencyplanner->setHand(contingencyplannerhand);
+                arrayofPlayers[i]=contingencyplanner;
+                break;
+            }
+            default:{
+                break;
+            }
+        }
+    }
+    
+    
+}
+
+void addEpCardsPlayerDeck(){
+    //insert Epidemic card after initial PlayerCard's were distributed to players
+    playerdeck.insert(playerdeck.end(), epcardarr, epcardarr+(sizeof(epcardarr)/sizeof(epcardarr[0])));
+    
+    //Rnd Shuffle PlayerDeck
+    shuffle(playerdeck.begin(), playerdeck.end(), std::default_random_engine(std::random_device()()));
+}
+void initialInfection() {
+    //draw 3, then 2, then 1 infection cards and infect according to their city and color
+    int i = 3;
+    while (i >= 1) {
+        for (int k = 3; k > 0; k--) {
+            //draw infection card from infection deck
+            InfectionCard* curr_inf = infectiondeck.back();
+            string city = curr_inf->getCardName();
+            string color = curr_inf->getCardTextFront();
+            //infect the city
+            for (int j = 1; j <= i; j++) {
+                curr_inf->Infect(remainingDiseaseCubes, city, color);
+            }
+            //add drawn card to discard pile
+            infectiondeck_discard.push_back(curr_inf);
+            //remove card from infection deck
+            infectiondeck.pop_back();
+        }
+        i--;
+    }
+    system("pause");
+}
+
+void initGame(){
+    
+    initInfectionDeck();
+    
+    setInitPlayerDeck();
+    
+    createRoles();
+    
+    addEpCardsPlayerDeck();
+    
+    initialInfection();
+    
+}
+
+void endGame(){
+    
+    // <vector> arrayofPlayerViews contains all *PlayerView (1 per player)
+    for(int i=0; i<nbplayers;i++){
+        arrayofPlayerViews[i]=nullptr;
+        delete arrayofPlayerViews[i];
+    }
+    
+    // <vector> arrayofPlayers contains all *Player
+    for(int i=0; i<nbplayers;i++){
+        arrayofPlayers[i]=nullptr;
+        delete arrayofPlayers[i];
+    }
+    
+    // <vector> playerdeck contains *PlayerCard
+    for(int i=0;i<playerdeck.size();i++){
+        playerdeck[i]=nullptr;
+        delete playerdeck[i];
+    }
+    // <vector> discardpile contains *Playercard
+    for(int i=0;i<discardpile.size();i++){
+        discardpile[i]=nullptr;
+        delete discardpile[i];
+    }
+    
+    // <vector> infectiondeck contains *InfectionCard
+    for (int i = 0; i<infectiondeck.size(); i++) {
+        infectiondeck[i] = nullptr;
+        delete infectiondeck[i];
+    }
+    // <vector> infectiondeck_discard contains *InfectionCard
+    for (int i = 0; i<infectiondeck_discard.size(); i++) {
+        infectiondeck_discard[i] = nullptr;
+        delete infectiondeck_discard[i];
+    }
+    
+    
+}
+
+
+
+
+int main(){
+    
+    initGame();
+    
+    //test lines (draw cards)
+    arrayofPlayers[0]->draw2pcards(playerdeck);
+    arrayofPlayers[0]->draw2pcards(playerdeck);
+    arrayofPlayers[0]->draw2pcards(playerdeck);
+    
+   // Save savestate = Save();
+   // savestate.save_game();
+   // system("pause");
+   // savestate.load_game();
+    endGame();
+}
