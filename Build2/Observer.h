6b07eba0a05aa66b4c5fbc53ec4004eea3f383ba
kawsara25 <kawsara25@yahoo.com>
2017-04-04 22:31:39 -0400

    Update Observer.h
    added an update map function for the map observer

--- Build2/Observer.h
+++ Build2/Observer.h
@@ -13,6 +13,7 @@ class Observer {
 public:
     ~Observer();
     virtual void Update() = 0;  //pure virtual function: must be implemented
+   virtual void UpdateMap() = 0;
     virtual void UpdateHand() = 0;
     virtual void UpdateDiscardHand()=0;
     virtual void UpdateEpidemicCard()=0;
