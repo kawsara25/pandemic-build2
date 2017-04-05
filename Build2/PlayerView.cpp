30869d70bdf484e66d4c26cf979b10e74239990a
kawsara25 <kawsara25@yahoo.com>
2017-04-04 22:45:05 -0400

    added map view

--- Build2/PlayerView.cpp
+++ Build2/PlayerView.cpp
@@ -80,3 +80,29 @@ void PlayerView::displayEpidemicCard(){
     ((p->getHand()).back())->printCard();
     
 }
+#include "GraphView.h"
+
+
+GraphView::GraphView()
+{
+}
+
+GraphView::GraphView(Graph* s)
+{
+	_subject = s;
+	_subject->Attach(this);
+}
+GraphView::~GraphView()
+{
+	_subject->Detach(this);
+}
+void GraphView::UpdateMap(){
+	//Called by Notify() when state of Subject changes
+	displayMap();
+};
+void GraphView::displayMap(){
+	//_subject->showCity();
+	_subject->cityWithAdjacency();
+	_subject->cityAndConnection();
+	
+}
